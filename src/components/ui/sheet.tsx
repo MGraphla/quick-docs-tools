898aae282a61676caf28e040615b605aa08a137b
MGraphla <89389697+MGraphla@users.noreply.github.com>
2025-06-29 12:37:35 +0100

    Fix Sheet component accessibility error

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -1,4 +1,5 @@
 import * as SheetPrimitive from "@radix-ui/react-dialog"
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden"
 import { cva, type VariantProps } from "class-variance-authority"
 import { X } from "lucide-react"
 import * as React from "react"
@@ -62,6 +63,9 @@ const SheetContent = React.forwardRef<
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
+      <SheetTitle>
+        <VisuallyHidden.Root>Sheet</VisuallyHidden.Root>
+      </SheetTitle>
       {children}
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="h-4 w-4" />
@@ -128,4 +132,3 @@ export {
   Sheet, SheetClose,
   SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetOverlay, SheetPortal, SheetTitle, SheetTrigger
 }
-
